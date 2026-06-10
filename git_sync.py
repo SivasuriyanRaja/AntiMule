72c43e8218a76e34786530d52326ad948abd8640
Sivasuriyan Raja <lcsb2363@krce.ac.in>
2026-06-10 19:42:53 +0530

    refactor: simplify git sync process by removing redundant local commits and adding allow-unrelated-histories flag during merge

--- git_sync.py
+++ git_sync.py
@@ -8,13 +8,5 @@ def run_cmd(cmd):
         print("STDERR:", res.stderr)
     return res.returncode
 
-# Make sure we don't have uncommitted changes first
-run_cmd('git add .')
-run_cmd('git commit -m "Auto-commit local changes before merge"')
-
-# Merge favoring our local optimized Vercel files
-run_cmd('git fetch origin')
-run_cmd('git merge origin/main -X ours -m "Merge remote deployment changes, favoring local optimizations"')
-
-# Push
+run_cmd('git merge origin/main --allow-unrelated-histories -X ours -m "Merge remote deployment changes, favoring local optimizations"')
 run_cmd('git push origin main')
